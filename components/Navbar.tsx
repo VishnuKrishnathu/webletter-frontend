562d3e496bc7087edbec70c28efd926d0630c1f4
Vishnu Krishnathu <vishnu.krishnathu1999@gmail.com>
2022-01-16 13:02:58 +0530

    Login functionality added
    Changes in the commit
        - login function added to login.tsx
        - Promotions banner removed from the layout
        - BlogsSection removed from the layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-
 export default function Navbar() {
     return (
         <div className='text-indigo-900 flex justify-between items-center py-1 shadow-md h-12'>
-            <div className='ml-8 font-bold text-3xl'>Logo</div>
+            <div className='ml-8 font-bold text-3xl cursor-pointer'>Logo</div>
             <div className='grow-[0.3] mr-8'>
                 <ul className='flex justify-between'>
                     <li><a href="#" className='hover:underline hover:font-bold'>Home</a></li>
                     <li><a href="#" className='hover:underline hover:font-bold'>Profile</a></li>
-                    <li><a href="#" className='hover:underline hover:font-bold'>Compose</a></li>
+                    <li><a href="compose-article" className='hover:underline hover:font-bold'>Compose</a></li>
                 </ul>
             </div>
         </div>
